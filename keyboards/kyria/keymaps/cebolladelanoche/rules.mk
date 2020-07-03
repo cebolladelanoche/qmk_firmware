900d555f9a6846b66e438dca655d46960e6b5d68
Chris Hall <cebolladelanoche@gmail.com>
2020-07-03 16:16:46 -0700

    added pipe and others

--- keyboards/kyria/keymaps/cebolladelanoche/rules.mk
+++ keyboards/kyria/keymaps/cebolladelanoche/rules.mk
@@ -1,3 +1,10 @@
-OLED_DRIVER_ENABLE = no   # Enables the use of OLED displays
-ENCODER_ENABLE = no       # ENables the use of one or more encoders
+EXTRAFLAGS += -flto
+
+OLED_DRIVER_ENABLE = yes   # Enables the use of OLED displays
 RGBLIGHT_ENABLE = yes      # Enable keyboard RGB underglow
+WPM_ENABLE = yes
+ENCODER_ENABLE = no       # ENables the use of one or more encoders
+UNICODE_ENABLE = no
+MOUSEKEY_ENABLE = no
+CONSOLE_ENABLE = no
+EXTRAKEY_ENABLE = no
